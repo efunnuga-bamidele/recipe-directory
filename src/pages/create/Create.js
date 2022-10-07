79b5c8e24124f66f2c4616fdca80131b0034b031
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-07 15:24:43 +0100

    🚀 Create list of ingredients array

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 //Styles
 import './Create.css'
@@ -7,12 +7,26 @@ export default function Create() {
     const [title, setTitle] = useState('')
     const [method, setMethod] = useState('')
     const [cookingTime, setCookingTime] = useState('')
+    const [newIngredient, setNewIngredient] = useState('')
+    const [ingredients, setIngredients] = useState([])
+    const ingredientInput = useRef(null)
 
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(title, method, cookingTime)
+        console.log(title, method, cookingTime, ingredients)
 
+    }
+
+    const handleAdd = (e) => {
+        e.preventDefault()
+        const ing = newIngredient.trim()
+
+        if (ing && !ingredients.includes(ing)){
+            setIngredients(prevIngredients => [...prevIngredients, ing])
+        }
+        setNewIngredient('')
+        ingredientInput.current.focus()
     }
 
     return(
@@ -29,6 +43,21 @@ export default function Create() {
                     required
                     />
                </label> 
+
+               <label>
+                <span>Recipe Ingredients:</span>
+                <div className='ingredients'>
+                    <input 
+                        type="text" 
+                        onChange={(e) => setNewIngredient(e.target.value)}
+                        value={newIngredient}
+                        ref={ingredientInput}
+                        />
+                    <button onClick={handleAdd} className="btn" >add</button>
+                </div>
+               </label>
+               <p>Current ingredients: {ingredients.map(i => <em key={i}>{i}, </em>)} </p>
+               
                <label>
                 <span>Recipe method:</span>
                 <textarea
