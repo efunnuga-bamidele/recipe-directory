b1ddeae37525c8cbd85d99272f699ae621933c37
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-07 15:05:21 +0100

    ✨ Create item click and Create New Recipe

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar(){
         <div className='navbar'>
             <nav>
                 <Link to="/" className='brand'>
-                    <h1>Cooking Ninja</h1>
+                    <h1>Cooking Recipe App</h1>
                 </Link>
                 <Link to='/create'>Create Recipe</Link>
 
