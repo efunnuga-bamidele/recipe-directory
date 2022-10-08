7193ff644d3ee703ecff4652a42a6f5982451d04
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-08 21:14:34 +0100

    ✨ Create Context API with useContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
+
+//components
 import Searchbar from './Searchbar'
 // Style
 import './Navbar.css'
 
+
 export default function Navbar(){
+    const { color } = useTheme()
+
     return(
-        <div className='navbar'>
+        <div className='navbar' style={{ background: color }}>
             <nav>
                 <Link to="/" className='brand'>
                     <h1>Cooking Recipe App</h1>
---
+++ src/context/ThemeContext.js
@@ -0,0 +1,15 @@
+import { createContext } from "react";
+
+export const ThemeContext = createContext()
+
+export function ThemeProvider({ children }) {
+
+    //custom logic
+
+    return(
+        
+  <ThemeContext.Provider value={{color: 'blue'}}>
+        {children}
+  </ThemeContext.Provider>
+    )
+}
