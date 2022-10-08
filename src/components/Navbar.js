2713271507a753e350d9b0a81a8514ac92eeef9f
Efunnuga Bamidele <bamideleefunnuga9@gmail.com>
2022-10-08 01:31:43 +0100

    ✨ Search recipe functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Searchbar from './Searchbar'
 // Style
 import './Navbar.css'
 
@@ -9,6 +10,7 @@ export default function Navbar(){
                 <Link to="/" className='brand'>
                     <h1>Cooking Recipe App</h1>
                 </Link>
+                <Searchbar />
                 <Link to='/create'>Create Recipe</Link>
 
             </nav>
